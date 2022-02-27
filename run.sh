7d3f4e4ebd97bb177c46dae7556569e19020b263
Jack <60232273+ACK-J@users.noreply.github.com>
2022-02-27 14:51:33 -0500

    bug fixes
    - added comments 
    - added priority chosen from distribution

--- run.sh
+++ run.sh
@@ -7,27 +7,25 @@
 
 # Usage: chmod +x ./run.sh && ./run.sh
 
-
-
-
-
 # Ask the user for a number of wallets to make
 read -p "How many wallets would you like (ex. 10)? " numwallets
-cd "./Wallets"
+cd "./Wallets" || exit
 # Create the directories which will store 2 wallets each
 for i in `seq $(($numwallets / 2))`; do mkdir $i; done
-cd -
+cd - || exit
+
+
 
 
 
 # Make 2 wallets per folder
-while read dir 
-do 
-	cd "$dir"
+while read dir
+do
+	cd "$dir" || exit
+	#  Run the scripts to make the two wallets
 	../MakeWallet1.exp
 	../MakeWallet2.exp
-	cd -
-	
+	cd - || exit
 done < <(find ./Wallets -mindepth 1 -type d | sort -u)
 
 
@@ -35,11 +33,12 @@ done < <(find ./Wallets -mindepth 1 -type d | sort -u)
 
 
 # Fund the wallets (This part is slow but no real way around it)
-while read walletFile; do 
-walletAddr=`cat "$walletFile"`
-cat > ./FundWallet.exp <<EOL 
+while read walletFile; do
+  walletAddr=$(cat "$walletFile") #  Get the wallet addr from the txt file
+  # Make a new expect script substituting the addr to fund the wallet
+  cat > ./FundWallet.exp <<EOL
 #!/usr/bin/expect -f
-set timeout -1 
+set timeout -1
 spawn monero-wallet-cli --testnet --wallet ./FundingWallet --daemon-address testnet.melo.tools:28081 --log-file /dev/null --trusted-daemon
 match_max 10000
 expect "Wallet password: "
@@ -53,21 +52,20 @@ expect {
         "Transaction successfully submitted*wallet*]:*" {send "exit\r"}
 
         "Error: *\[wallet*" {sleep 1;send "transfer $walletAddr 0.35\r";exp_continue}
-	
+
         "(out of sync)]: *" {send "refresh\r";exp_continue}
-                               
+
         "Is this okay?  (Y/Yes/N/No): *"  {send "y\r";exp_continue}
-	
-	timeout {send "transfer $walletAddr \$amount\r";exp_continue} 
-              
+
+	timeout {send "transfer $walletAddr \$amount\r";exp_continue}
+
 }
 expect eof
 EOL
-
-chmod 777 ./FundWallet.exp && ./FundWallet.exp
-echo "Wallet $walletFile Funded!" && date
-sleep 600 # Wait 10 minutes instead of 20
-	
+  #  Run the script
+  chmod 777 ./FundWallet.exp && ./FundWallet.exp
+  echo "Wallet $walletFile Funded!" && date
+  sleep 600 # Wait 10 minutes instead of 20
 done < <(find ./Wallets/ -type f -name "*.txt" | sort -u)
 
 
@@ -76,28 +74,27 @@ done < <(find ./Wallets/ -type f -name "*.txt" | sort -u)
 
 # Start Transfers
 while read dir ;do  # Loop each directory
-	cd "$dir"
+	cd "$dir" || exit
 	while read walletAddrFile; do  # Loop each .txt wallet addr file
-	
 		#  Gets the name of the current wallet file
-		walletName=`echo $walletAddrFile | cut -f 2 -d "." | cut -f 2 -d "/"`
+		walletName=$(echo $walletAddrFile | cut -f 2 -d "." | cut -f 2 -d "/")
 		#  Since we want to transfer to the other wallet we need to switch the numbers
-		
+		#  Wallet1 transfers to Wallet2 and Wallet2 transfers to Wallet1
 		if [ "${walletName: -1}" -eq "2" ];then  # check if the last number of the wallet is a 2
 			recvWalletFile="${walletAddrFile//[2]/1}"  # Swap it with a 1
 		else  # The last num is a 1 -> swap it to a 2
 			recvWalletFile="${walletAddrFile//[1]/2}"
 		fi
-		walletAddr=`cat "$recvWalletFile"`
+		walletAddr=$(cat "$recvWalletFile")
 
-# Write an expect script
-cat > ./$walletName-spend.exp <<EOL 
+    # Write an expect script substituting the wallet name and addr
+    cat > ./$walletName-spend.exp <<EOL
 #!/usr/bin/expect -f
 if {[llength \$argv] != 2} {
   puts stderr "Usage: Pass an amount and a priority as arguments!"
   exit 1
 }
-set timeout 90
+set timeout 200
 set amount [lindex \$argv 0];   # 0.0001 -> .000000000001
 set priority [lindex \$argv 1];   # 0 -> 4
 spawn monero-wallet-cli --testnet --wallet ./$walletName --daemon-address testnet.melo.tools:28081 --log-file /dev/null --trusted-daemon
@@ -114,22 +111,24 @@ send -- "transfer $walletAddr \$amount\r"
 expect {
 
         "Transaction successfully submitted*wallet*]:*" {send "exit\r"}
-	
+
         "Error: *\[wallet*" {sleep 15;send "transfer $walletAddr \$amount\r";exp_continue}
-	
+
         "(out of sync)]: *" {send "refresh\r";exp_continue}
-                               
+
         "Is this okay?  (Y/Yes/N/No): *"  {send "y\r";exp_continue}
-	
+
 	timeout {send "transfer $walletAddr \$amount\r";exp_continue}
-        
+
 }
 expect eof
 EOL
 		chmod 777 ./$walletName-spend.exp
-		#  Open a new terminal tab -> Run the expect script to send a transaction of a random amount -> print time / transaction # -> sleep a random time selected from gamma distribution
-		xfce4-terminal --tab -x /bin/bash -c "i=1; while : ;do ./${walletName}-spend.exp \$(python3 -c \"import random;print(format(random.uniform(0.0001, 0.000000000001), '.12f'))\") \`python3 -c \"import random;print(random.choices([0,1,2,3,4], weights=[18,18,18,18,10], k=1)[0])\"\`; date; echo -en '\033[34mNumber of successful transactions: \033[0m'; echo \$i; ((i++)); python3 ../../Gamma.py; done"
+		#  A one-liner that took forever to make because nested string interpolation is a pain
+		#  Open a new terminal tab -> Run the script to send a transaction of a random amount and priority taken from real user distribution -> print time / transaction # -> sleep a random time selected from gamma distribution -> repeat
+		xfce4-terminal --tab -x /bin/bash -c "i=1; while : ;do ./${walletName}-spend.exp \$(python3 -c \"import random;print(format(random.uniform(0.0001, 0.000000000001), '.12f'))\") \`python3 select_transaction_priority.py\`; date; echo -en '\033[34mNumber of successful transactions: \033[0m'; echo \$i; ((i++)); python3 ../../Gamma.py; done"
+		#  A delay of opening a new tab to not overload the server. Most wallets will have to scan the network for a while before transacting
 		sleep 60
 	done < <(find ./ -type f -name "*.txt" | sort -u)
-	cd - # Reset the directory
+	cd - || exit # Reset the directory
 done < <(find ./Wallets -mindepth 1 -type d | sort -u) 
