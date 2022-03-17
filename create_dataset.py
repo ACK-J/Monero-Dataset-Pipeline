2213552a831fbf1370f355048daafcde5d164269
Jack <60232273+ACK-J@users.noreply.github.com>
2022-03-17 01:29:38 -0400

    Added features
    added input and output features
    combined the rest of the files

--- create_dataset.py
+++ create_dataset.py
@@ -2,9 +2,11 @@ import os
 import pickle
 import time
 import requests
+from datetime import datetime
 
 # TODO add checkpoint saves
 # TODO add tx inputs and decoy info
+# TODO timestamps cant be subtracted need to use a library
 
 # Key = tx hash, val = dict(transaction metadata)
 data = {}
@@ -41,31 +43,49 @@ def enrich_data():
             data[txid]['Total_Block_Fee'] = float(total_block_fee * 0.000000000001) #  Converted from piconero to monero
             data[txid]['Block_Size'] = block_response["size"]
             data[txid]['Time_Since_Last_Block'] = int(block_response["timestamp"]) - int(previous_block_response["timestamp"])
+
+            #  Add Output Information
+            data[txid]['Outputs'] = []
+            for output in tx_response['outputs']:
+                data[txid]['Outputs'].append({'Amount': output['amount'], 'Stealth_Address': output['public_key']})
+
+            #  Add Input Information
+            data[txid]['Inputs'] = []
+            for input in tx_response['inputs']:
+                data[txid]['Inputs'].append(
+                    {
+                        'Amount': input['amount'],
+                        'Key_Image': input['key_image'],
+                        'Mixins': input['mixins']
+                    }
+                )
+            pass
         except Exception as e:
             print(e)
 
 
 def combine_files(Wallet_addrs):
+    #  Most function calls will input a list of 2 wallet addresses
     for addr in Wallet_addrs:
         with open("./cli_export_" + addr + ".csv", "r") as fp:
             next(fp)  # Skip header of csv
             for line in fp:
-                values = line.split(",")
-                if values[1].strip() == "out": #  Only add outgoing transactions to the dataset
+                cli_csv_values = line.split(",")
+                if cli_csv_values[1].strip() == "out": #  Only add outgoing transactions to the dataset
                     transaction = {}
-                    transaction['Tx_Hash'] = values[6].strip()
-                    transaction['Block_Number'] = int(values[0].strip())
-                    transaction['Direction'] = values[1].strip()
-                    transaction['Tx_Timestamp'] = values[3].strip()
-                    transaction['Amount'] = float(values[4].strip())
-                    transaction['Wallet_Balance'] = float(values[5].strip())
-                    transaction['Tx_Fee'] = float(values[8].strip())
-                    transaction['Destination_Address'] = values[9].strip()
+                    transaction['Tx_Hash'] = cli_csv_values[6].strip()
+                    transaction['Block_Number'] = int(cli_csv_values[0].strip())
+                    transaction['Direction'] = cli_csv_values[1].strip()
+                    transaction['Tx_Timestamp'] = cli_csv_values[3].strip()
+                    transaction['Amount'] = float(cli_csv_values[4].strip())
+                    transaction['Wallet_Balance'] = float(cli_csv_values[5].strip())
+                    transaction['Tx_Fee'] = float(cli_csv_values[8].strip())
+                    transaction['Destination_Address'] = cli_csv_values[9].strip()
                     transaction['Network'] = NETWORK
 
-                    with open("./xmr2csv_start_time_" + addr + ".csv", "r") as fp:
-                        for line in fp:
-                            transaction['xmr2csv_Data_Collection_Time'] = int(line.strip())
+                    with open("./xmr2csv_start_time_" + addr + ".csv", "r") as fp2:
+                        for line2 in fp2:
+                            transaction['xmr2csv_Data_Collection_Time'] = int(line2.strip())
                             break
 
                     if transaction['Tx_Hash'] not in data:
@@ -74,86 +94,70 @@ def combine_files(Wallet_addrs):
         with open("./xmr_report_" + addr + ".csv", "r") as fp:
             next(fp)  # Skip header of csv
             for line in fp:
-                values = line.split(",")
-                if values[2].strip() in data:
-                    data[values[2].strip()]['Tx_Version'] = values[4].strip()
-                    data[values[2].strip()]['Tx_Public_Key'] = values[3].strip()
-                    data[values[2].strip()]['Output_Pub_Key'] = values[8].strip()
-                    data[values[2].strip()]['Output_Key_Img'] = values[9].strip()
-                    data[values[2].strip()]['Output_Number_Spent'] = values[10].strip()
-                # else:
-                #     transaction = {}
-                #     transaction['Tx_Hash'] = values[2].strip()
-                #     transaction['Tx_Version'] = values[4].strip()
-                #     transaction['Tx_Public_Key'] = values[3].strip()
-                #     transaction['Output_Pub_Key'] = values[8].strip()
-                #     transaction['Output_Key_Img'] = values[9].strip()
-                #     transaction['Output_Number_Spent'] = values[10].strip()
-                #     with open("./xmr2csv_start_time_" + addr + ".csv", "r") as fp:
-                #         for line in fp:
-                #             transaction['xmr2csv_Data_Collection_Time'] = int(line.strip())
-                #             break
-                #     data[values[2].strip()] = transaction
+                xmr2csv_report_csv_values = line.split(",")
+                #  Check if the tx hash is in the dataset yet
+                if xmr2csv_report_csv_values[2].strip() in data:
+                    data[xmr2csv_report_csv_values[2].strip()]['Tx_Version'] = xmr2csv_report_csv_values[4].strip()
+                    data[xmr2csv_report_csv_values[2].strip()]['Tx_Public_Key'] = xmr2csv_report_csv_values[3].strip()
+                    data[xmr2csv_report_csv_values[2].strip()]['Output_Pub_Key'] = xmr2csv_report_csv_values[8].strip()
+                    data[xmr2csv_report_csv_values[2].strip()]['Output_Key_Img'] = xmr2csv_report_csv_values[9].strip()
+                    data[xmr2csv_report_csv_values[2].strip()]['Output_Number_Spent'] = xmr2csv_report_csv_values[10].strip()
+
+                    #  Search through the export of all ring member occurrences on chain to see if our output public key was used
+                    data[xmr2csv_report_csv_values[2].strip()]['Ring_Members_OnChain'] = []
+                    with open("./xmr_report_ring_members_" + addr + ".csv", "r") as fp2:
+                        next(fp2)  # Skip header of csv
+                        for line2 in fp2:
+                            ring_members_csv_values = line2.split(",")
+                            Ring_Member = {}
+                            #  Make sure the ring members public key matches the current public key
+                            if data[xmr2csv_report_csv_values[2].strip()]['Output_Pub_Key'] == ring_members_csv_values[3].strip():
+                                Ring_Member['Tx_Hash'] = ring_members_csv_values[2].strip()
+                                Ring_Member['Block_Number'] = ring_members_csv_values[1].strip()
+                                # Convert timestamp to epoch time before saving
+                                p = "%Y-%m-%d %H:%M:%S"
+                                epoch = datetime(1970, 1, 1)
+                                ring_member_epoch_time = int((datetime.strptime(ring_members_csv_values[0].strip(), p) - epoch).total_seconds())
+                                Ring_Member['Tx_Timestamp'] = ring_member_epoch_time
+                                Ring_Member['Key_image'] = ring_members_csv_values[4].strip()
+                                Ring_Member['Ring_no/Ring_size'] = ring_members_csv_values[5].strip()
+                                #  Find the relative age of the outputs public key on the chain compared to when xmr2csv was ran
+                                #  https://stackoverflow.com/questions/30468371/how-to-convert-python-timestamp-string-to-epoch
+                                Ring_Member['Ring_Member_Relative_Age'] = data[xmr2csv_report_csv_values[2].strip()]['xmr2csv_Data_Collection_Time'] - Ring_Member['Tx_Timestamp']
+                                with open("./xmr_report_ring_members_freq_" + addr + ".csv", "r") as fp3:
+                                    next(fp3)  # Skip header of csv
+                                    for line3 in fp3:
+                                        ring_member_freq_csv_values = line3.split(",")
+                                        #  Check if the ring members public key matches the current public key
+                                        if data[xmr2csv_report_csv_values[2].strip()]['Output_Pub_Key'] == ring_member_freq_csv_values[0].strip():
+                                            #  Add the amount of times it has been seen on chain
+                                            Ring_Member['Ring_Member_Freq'] = int(ring_member_freq_csv_values[1].strip())
+                                data[xmr2csv_report_csv_values[2].strip()]['Ring_Members_OnChain'].append(Ring_Member)
+
+                    with open("./xmr2csv_start_time_" + addr + ".csv", "r") as fp2:
+                        for line2 in fp2:
+                            data[xmr2csv_report_csv_values[2].strip()]['xmr2csv_Data_Collection_Time'] = int(line2.strip())
+                            break
 
         with open("./xmr_report_outgoing_txs_" + addr + ".csv", "r") as fp:
             next(fp)  # Skip header of csv
             for line in fp:
-                values = line.split(",")
-                if values[2].strip() in data:
-                    data[values[2].strip()]['Ring_no/Ring_size'] = values[5].strip()
+                xmr2csv_outgoing_csv_values = line.split(",")
+                if xmr2csv_outgoing_csv_values[2].strip() in data:
+                    data[xmr2csv_outgoing_csv_values[2].strip()]['Ring_no/Ring_size'] = xmr2csv_outgoing_csv_values[5].strip()
                 else:
                     transaction = {}
-                    transaction['Tx_Hash'] = values[2].strip()
-                    transaction['Tx_Timestamp'] = values[0].strip()
-                    transaction['Block_Number'] = values[1].strip()
-                    transaction['Output_Pub_Key'] = values[3].strip()
-                    transaction['Output_Key_Img'] = values[4].strip()
-                    transaction['Ring_no/Ring_size'] = values[5].strip()
-                    with open("./xmr2csv_start_time_" + addr + ".csv", "r") as fp:
-                        for line in fp:
-                            transaction['xmr2csv_Data_Collection_Time'] = int(line.strip())
+                    transaction['Tx_Hash'] = xmr2csv_outgoing_csv_values[2].strip()
+                    transaction['Tx_Timestamp'] = xmr2csv_outgoing_csv_values[0].strip()
+                    transaction['Block_Number'] = xmr2csv_outgoing_csv_values[1].strip()
+                    transaction['Output_Pub_Key'] = xmr2csv_outgoing_csv_values[3].strip()
+                    transaction['Output_Key_Img'] = xmr2csv_outgoing_csv_values[4].strip()
+                    transaction['Ring_no/Ring_size'] = xmr2csv_outgoing_csv_values[5].strip()
+                    with open("./xmr2csv_start_time_" + addr + ".csv", "r") as fp2:
+                        for line2 in fp2:
+                            transaction['xmr2csv_Data_Collection_Time'] = int(line2.strip())
                             break
-                    data[values[2].strip()] = transaction
-
-        # with open("./xmr_report_ring_members_" + addr + ".csv", "r") as fp:
-        #     next(fp)  # Skip header of csv
-        #     for line in fp:
-        #         values = line.split(",")
-        #         if values[2].strip() in data:
-        #             data[values[2].strip()]['Ring_no/Ring_size'] = values[5].strip()
-        #         else:
-        #             transaction = {}
-        #             transaction['Tx_Hash'] = values[2].strip()
-        #             transaction['Tx_Timestamp'] = values[0].strip()
-        #             transaction['Block_Number'] = values[1].strip()
-        #             transaction['Output_Pub_Key'] = values[3].strip()
-        #             transaction['Output_Key_Img'] = values[4].strip()
-        #             transaction['Ring_no/Ring_size'] = values[5].strip()
-        #             with open("./xmr2csv_start_time_" + addr + ".csv", "r") as fp:
-        #                 for line in fp:
-        #                     transaction['xmr2csv_Data_Collection_Time'] = int(line.strip())
-        #                     break
-        #             data[values[2].strip()] = transaction
-
-        # with open("./xmr_report_ring_members_freq_" + addr + ".csv", "r") as fp:
-        #     next(fp)  # Skip header of csv
-        #     for line in fp:
-        #         values = line.split(",")
-        #         if values[2].strip() in data:
-        #             data[values[2].strip()]['Ring_no/Ring_size'] = values[5].strip()
-        #         else:
-        #             transaction = {}
-        #             transaction['Tx_Hash'] = values[2].strip()
-        #             transaction['Tx_Timestamp'] = values[0].strip()
-        #             transaction['Block_Number'] = values[1].strip()
-        #             transaction['Output_Pub_Key'] = values[3].strip()
-        #             transaction['Output_Key_Img'] = values[4].strip()
-        #             transaction['Ring_no/Ring_size'] = values[5].strip()
-        #             data[values[2].strip()] = transaction
-        #             with open("./xmr2csv_start_time_" + addr + ".csv", "r") as fp:
-        #                 for line in fp:
-        #                     transaction['xmr2csv_Data_Collection_Time'] = int(line.strip())
-        #                     break
+                    data[xmr2csv_outgoing_csv_values[2].strip()] = transaction
 
 
 def discover_wallet_directories():
