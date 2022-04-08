1a3d26ec23dbbf496eb46295de089b5c2c610dd2
Jack <60232273+ACK-J@users.noreply.github.com>
2022-04-08 16:52:22 -0400

    Update Gamma.py

--- Gamma.py
+++ Gamma.py
@@ -5,9 +5,9 @@ shape=19.28
 rate=1.61
 #  Sleep a random value chosen at random from a gamma dist + 1200 seconds for the 20 min lockout
 sample=int(exp(np.random.gamma(shape, 1.0/rate, 1))) + 1200
-if sample > 43200:
-	print("Sleep for ", 43200)
-	sleep(43200)
+if sample > 86400:
+	print("Sleep for ", 86400)
+	sleep(86400)
 else:
 	print("Sleep for ", sample)
 	sleep(sample)
