- fixed charts plotting precision - reworked neural network

DataScience/GradientBoostedClassifier.py
@@ -8,6 +8,7 @@ from tqdm import tqdm
 from multiprocessing import cpu_count, Manager
 from sklearn.metrics import f1_score
 from itertools import repeat
+import numpy as np
 
 #  Colors
 GREEN = '\033[92m'
@@ -59,6 +60,7 @@ def run_model(X_train, X_test, y_train, y_test, RANDOM_STATE, X_Validation, y_Va
 
     if stagenet:
         cm = confusion_matrix(y_test, y_pred)
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
         #  Heat map
         plt.figure(figsize=(10, 7))
         sn.heatmap(cm, annot=True)
@@ -67,6 +69,7 @@ def run_model(X_train, X_test, y_train, y_test, RANDOM_STATE, X_Validation, y_Va
         plt.savefig("./models/GBC/stagenet/CM_num_estimators_" + str(NUM_ESTIMATORS) + "_lr_" + str(LR) + "_seed_" + str(RANDOM_STATE) + "_accuracy_" + '{:.2f}'.format(weighted_f1) + ".png")
     else:
         cm = confusion_matrix(y_test, y_pred)
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
         #  Heat map
         plt.figure(figsize=(10, 7))
         sn.heatmap(cm, annot=True)
@@ -81,6 +84,7 @@ def run_model(X_train, X_test, y_train, y_test, RANDOM_STATE, X_Validation, y_Va
 
     if stagenet:
         cm = confusion_matrix(y_Validation, y_main_predict)
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
         #  Heat map
         plt.figure(figsize=(10, 7))
         sn.heatmap(cm, annot=True)
@@ -89,6 +93,7 @@ def run_model(X_train, X_test, y_train, y_test, RANDOM_STATE, X_Validation, y_Va
         plt.savefig("./models/GBC/stagenet/Main_CM_num_estimators_" + str(NUM_ESTIMATORS) + "_lr_" + str(LR) + "_seed_" + str(RANDOM_STATE) + "_accuracy_" + '{:.2f}'.format(weighted_f1_mainnet) +  ".png")
     else:
         cm = confusion_matrix(y_Validation, y_main_predict)
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
         #  Heat map
         plt.figure(figsize=(10, 7))
         sn.heatmap(cm, annot=True)
@@ -112,7 +117,7 @@ def gradient_boosted(X_train, X_test, y_train, y_test, RANDOM_STATE, X_Validatio
 
     NUM_PROCESSES = cpu_count()
 
-    Num_Iterations = 5
+    Num_Iterations = 10
 
     if NUM_PROCESSES > Num_Iterations:
         NUM_PROCESSES = Num_Iterations

DataScience/NeuralNetwork.py
@@ -62,8 +62,8 @@ def MLP(X_train, X_test, y_train, y_test, X_Validation, y_Validation, stagenet=T
     out_of_sample_f1 = []
     mainnet_f1 = []
 
-    EPOCHS = 30
-    BATCH_SIZE = 256
+    EPOCHS = 100
+    BATCH_SIZE = 512
 
     y_test_copy = y_test.copy()
     y_test = np.asarray(y_test)
@@ -89,20 +89,20 @@ def MLP(X_train, X_test, y_train, y_test, X_Validation, y_Validation, stagenet=T
     scaler = StandardScaler().fit(X_Validation)
     X_Validation = scaler.transform(X_Validation)
 
-    for i in range(5):
+    for i in range(10):
         from keras_visualizer import visualizer
         model = Sequential()
         model.add(Dense(11, input_shape=(X_train.shape[1],), activation='relu'))
-        model.add(Dense(32, activation='relu'))
+        #model.add(Dense(32, activation='relu'))
         model.add(Dense(64, activation='relu'))
-        model.add(Dense(128, activation='relu'))
+        #model.add(Dense(128, activation='relu'))
         model.add(Dropout(.1))
-        model.add(Dense(256, activation='relu'))
-        model.add(Dense(128, activation='relu'))
+        #model.add(Dense(256, activation='relu'))
+        #model.add(Dense(128, activation='relu'))
         model.add(Dense(64, activation='relu'))
-        model.add(Dropout(.3))
+        #model.add(Dropout(.3))
         model.add(Dense(32, activation='relu'))
-        model.add(Dropout(.2))
+        #model.add(Dropout(.2))
         model.add(Dense(11))
         model.add(BatchNormalization())
         model.add(Activation('softmax'))
@@ -110,20 +110,20 @@ def MLP(X_train, X_test, y_train, y_test, X_Validation, y_Validation, stagenet=T
         # https://towardsdatascience.com/visualizing-keras-models-4d0063c8805e
         # visualizer(model, format='png', view=True)
 
-        class_weight = {0: 1.,
-                        1: 50.,
-                        2: 50.,
-                        3: 50.,
-                        4: 50.,
-                        5: 50.,
-                        6: 50.,
-                        7: 50.,
-                        8: 50.,
-                        9: 50.,
-                        10: 1.}
+        # class_weight = {0: 1.,
+        #                 1: 50.,
+        #                 2: 50.,
+        #                 3: 50.,
+        #                 4: 50.,
+        #                 5: 50.,
+        #                 6: 50.,
+        #                 7: 50.,
+        #                 8: 50.,
+        #                 9: 50.,
+        #                 10: 1.}
 
         model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-        model.fit(X_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, validation_data=(X_Validation, y_val), class_weight=class_weight)
+        model.fit(X_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, validation_data=(X_Validation, y_val))#, class_weight=class_weight)
         if stagenet:
             with open("./models/NN/stagenet/epochs_" + str(EPOCHS) + "_batch_size_" + str(BATCH_SIZE) + "_i_" + str(i) + ".pkl", "wb") as fp:
                 pickle.dump(model, fp)
@@ -171,6 +171,7 @@ def MLP(X_train, X_test, y_train, y_test, X_Validation, y_Validation, stagenet=T
 
         if stagenet:
             cm = confusion_matrix(y_val_copy, y_main_predict)
+            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
             #  Heat map
             plt.figure(figsize=(10, 7))
             sn.heatmap(cm, annot=True)
@@ -179,6 +180,7 @@ def MLP(X_train, X_test, y_train, y_test, X_Validation, y_Validation, stagenet=T
             plt.savefig("./models/NN/stagenet/MAIN_CM_epochs_" + str(EPOCHS) + "_batch_size_" + str(BATCH_SIZE) + "_i_" + str(i) + "_accuracy_" + str(weighted_f1_mainnet) + ".png")
         else:
             cm = confusion_matrix(y_val_copy, y_main_predict)
+            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
             #  Heat map
             plt.figure(figsize=(10, 7))
             sn.heatmap(cm, annot=True)

DataScience/RandomForest.py
@@ -9,6 +9,7 @@ from multiprocessing import cpu_count, Manager
 import pickle
 from itertools import repeat
 import matplotlib.pyplot as plt
+import numpy as np
 #  Colors
 GREEN = '\033[92m'
 RED = '\033[91m'
@@ -53,6 +54,25 @@ def run_model_rf(X_train, X_test, y_train, y_test, RANDOM_STATE, X_Validation, y
     print('Weighted F1-score: {:.2f}'.format(weighted_f1))
     #out_of_sample_f1.append(weighted_f1)
 
+    if stagenet:
+        cm = confusion_matrix(y_test, y_pred)
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        #  Heat map
+        plt.figure(figsize=(10, 7))
+        sn.heatmap(cm, annot=True)
+        plt.xlabel('Predicted')
+        plt.ylabel('Truth')
+        plt.savefig("./models/RF/stagenet/CM_num_estimators_" + str(N_ESTIMATORS) + "_seed_" + str(RANDOM_STATE) + "_accuracy_" + '{:.2f}'.format(weighted_f1) + ".png")
+    else:
+        cm = confusion_matrix(y_test, y_pred)
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        #  Heat map
+        plt.figure(figsize=(10, 7))
+        sn.heatmap(cm, annot=True)
+        plt.xlabel('Predicted')
+        plt.ylabel('Truth')
+        plt.savefig("./models/RF/testnet/CM_num_estimators_" + str(N_ESTIMATORS) + "_seed_" + str(RANDOM_STATE) + "_accuracy_" + '{:.2f}'.format(weighted_f1) + ".png")
+
     y_main_predict = model.predict(X_Validation)
     weighted_f1_mainnet = f1_score(y_Validation, y_main_predict, average='weighted')
     print('Mainnet Weighted F1-score: {:.2f}'.format(weighted_f1_mainnet))
@@ -60,20 +80,22 @@ def run_model_rf(X_train, X_test, y_train, y_test, RANDOM_STATE, X_Validation, y
 
     if stagenet:
         cm = confusion_matrix(y_Validation, y_main_predict)
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
         #  Heat map
         plt.figure(figsize=(10, 7))
         sn.heatmap(cm, annot=True)
         plt.xlabel('Predicted')
         plt.ylabel('Truth')
-        plt.savefig("./models/RF/stagenet/CM_num_estimators_" + str(N_ESTIMATORS) + "_seed_" + str(RANDOM_STATE) + "_accuracy_" + '{:.2f}'.format(weighted_f1_mainnet) + ".png")
+        plt.savefig("./models/RF/stagenet/MAIN_CM_num_estimators_" + str(N_ESTIMATORS) + "_seed_" + str(RANDOM_STATE) + "_accuracy_" + '{:.2f}'.format(weighted_f1_mainnet) + ".png")
     else:
         cm = confusion_matrix(y_Validation, y_main_predict)
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
         #  Heat map
         plt.figure(figsize=(10, 7))
         sn.heatmap(cm, annot=True)
         plt.xlabel('Predicted')
         plt.ylabel('Truth')
-        plt.savefig("./models/RF/testnet/CM_num_estimators_" + str(N_ESTIMATORS) + "_seed_" + str(RANDOM_STATE) + "_accuracy_" + '{:.2f}'.format(weighted_f1_mainnet) + ".png")
+        plt.savefig("./models/RF/testnet/MAIN_CM_num_estimators_" + str(N_ESTIMATORS) + "_seed_" + str(RANDOM_STATE) + "_accuracy_" + '{:.2f}'.format(weighted_f1_mainnet) + ".png")
     return weighted_f1, weighted_f1_mainnet
 
 
@@ -88,7 +110,7 @@ def random_forest(X_train, X_test, y_train, y_test, N_ESTIMATORS, MAX_DEPTH, RAN
     mainnet_f1 = []
 
     NUM_PROCESSES = cpu_count()
-    Num_Iterations = 5
+    Num_Iterations = 10
 
     if NUM_PROCESSES > Num_Iterations:
         NUM_PROCESSES = Num_Iterations